1a3cda35ea01d464a2e56a5c1a8fb028a1daa9e2
silvia ferreira <silviacardoso21@gmail.com>
2022-08-30 17:34:08 +0100

    changes pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ r = Restaurant.create(name: "Vegan Junkies", owner: "Felicite", location: "near 
 u = User.create(email: "ealhaverkort@gmail.com", password: "1234567890")
 UserRestaurant.create(user_id: u.id, restaurant_id: r.id) #or (user: u, restaurant: r)
 m = Menu.create(name: "Lunch menu", category: "lunch", restaurant_id: r.id)
+
