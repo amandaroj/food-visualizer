a5a61cbcf9fa70d5a7298a9d47cbd9109864c5c7
Eva Haverkort <ealhaverkort@gmail.com>
2022-09-05 13:15:46 +0100

    review page done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_103935) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_094900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_103935) do
     t.bigint "menu_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "average_rating", default: "0.0"
     t.index ["menu_id"], name: "index_dishes_on_menu_id"
   end
 
