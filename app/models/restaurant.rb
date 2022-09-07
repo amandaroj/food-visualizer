41af0decf0b99cd722f62febbf0474171bc449bc
Joana Neuenschwander <jo.neush@gmail.com>
2022-09-07 11:12:55 +0100

    Made some changes to the dashboard

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :menus, dependent: :destroy
   has_many :user_restaurants, dependent: :destroy
   has_many :users, through: :user_restaurants
-  has_many_attached :photos
+  has_one_attached :photo
 end
