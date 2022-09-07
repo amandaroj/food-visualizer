6fd77b725ff36076da9cc016efd635968696eb49
Eva Haverkort <ealhaverkort@gmail.com>
2022-09-07 14:41:25 +0100

    fixed carousel and busy on the dashbaord

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,25 +17,23 @@ class ReviewsController < ApplicationController
     @dish = Dish.find(params[:dish_id])
     @review = Review.new(review_params)
     @review.dish_id = @dish.id
-    respond_to do |format|
-      if @review.save
-        Dish.update(@dish.id, :average_rating => ((@dish.average_rating + @review.rating)/Review.where(dish: @dish).size))
-        @review.concerned_users.each do |user|
-            ReviewChannel.broadcast_to(
-              user,
-              render_to_string(partial: "review", locals: {review: @review}) # this review is the one that is just saved
-            )
-            if @review.rating < 3
-              AlertChannel.broadcast_to(
-                user,
-                render_to_string(partial: "bad_review", locals: {review: @review})
-              )
-            end
-          end
-          redirect_to dishes_path(@menu.id)
-      else
-        render :new, state: :unprocessable_entities
+    if @review.save
+      Dish.update(@dish.id, :average_rating => ((@dish.average_rating + @review.rating)/Review.where(dish: @dish).size))
+      @review.concerned_users.each do |user|
+        ReviewChannel.broadcast_to(
+          user,
+          render_to_string(partial: "review", locals: {review: @review}) # this review is the one that is just saved
+        )
+        if @review.rating < 3
+          AlertChannel.broadcast_to(
+            user,
+            render_to_string(partial: "bad_review", locals: {review: @review})
+          )
+        end
       end
+      redirect_to dishes_path(@menu.id)
+    else
+      render :new, state: :unprocessable_entities
     end
   end
 
