c5cd5c5b029a2b7bc99774ae290a41a30eea8588
Eva Haverkort <ealhaverkort@gmail.com>
2022-09-08 14:26:06 +0100

    fix js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,9 +7,6 @@ import { application } from "./application"
 import AlertsSubscriptionController from "./alerts_subscription_controller"
 application.register("alerts-subscription", AlertsSubscriptionController)
 
-import FilterDishTypeController from "./filter_dish_type_controller"
-application.register("filter-dish-type", FilterDishTypeController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
